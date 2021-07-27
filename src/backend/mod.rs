d82415308e11a316f851a70f9e70db57a212d726
Monlih <MonliH@users.noreply.github.com>
2021-07-26 22:27:59 -0400

    Strip down to parser

--- src/backend/mod.rs
+++
@@ -1,4 +0,0 @@
-mod c_builder;
-mod c_compile;
-pub use c_builder::CBuilder;
-pub use c_compile::Compile;
--- src/middle/mod.rs
+++ src/middle/mod.rs
@@ -1,5 +1,3 @@
 mod interner;
-mod lower;
 mod optimize_ast;
 pub use interner::{Interner, StrId};
-pub use lower::LowerCompiler;
